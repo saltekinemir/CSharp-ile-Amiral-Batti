607e6371483e978ce5483ddd81f521b4e4d77654
saltekinemir <emirsaltekin@stu.khas.edu.tr>
2023-01-09 11:44:48 +0300

    BattleShip

---
+++ BATTLE-Ship/BATTLE-Ship/Player.cs
@@ -0,0 +1,25 @@
+﻿class Player
+{
+    public string name;
+    public int score;
+
+    public Player(int inScore)
+    {//Constructor
+
+
+        score = inScore;
+
+
+    }
+
+    public Player(string inName, int inScore)
+    {//Constructor
+
+        name = inName;
+        score = inScore;
+
+
+    }
+}
+
+
